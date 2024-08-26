43dc67ef0be967a0c15710f5b6493e0c870be1a0
jinjae84 <jinjae84@naver.com>
2024-08-26 21:43:47 +0900

    (2024.08.26) 아스키 코드

---
+++ Class 12th (Ascii Code)/Source.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+void Execute(int count)
+{
+	if (count <= 0)
+	{
+		return;
+	}
+
+	printf("Execute\n");
+	Execute(count - 1);
+	printf("%d\n", count);
+}
+
+int main()
+{
+#pragma region 재귀 함수
+	// 어떤 함수에서 자신을 다시 호출하여 작업을
+	// 수행하는 함수입니다.
+
+	// Execute(5);
+
+	// 재귀 함수는 함수를 계속 호출하기 때문에 스택 영역에
+	// 메모리가 계속 쌓이게 되므로 스택 오버플로우가 발생하게 됩니다.
+#pragma endregion
+
+#pragma region ASCII(아스키) 코드
+	// 미국 국립 표준 협회에서 표준화한 정보 교환용
+	// 7bit 부호 체계입니다.
+
+	// int alphabet = 'A';
+	// 
+	// // printf("alphabet 변수의 값 : %d\n", alphabet);
+	// // printf("alphabet 변수의 값 : %c\n", 'A');
+	// 
+	// for (int i = 0; i < 26; i++)
+	// {
+	// 	printf("%c ",alphabet + i);
+	// }
+
+#pragma endregion
+
+#pragma region 대소문자 변환
+
+	// char content[] = "League of Legend";
+	// 
+	// for (int i = 0; i < sizeof(content); i++)
+	// {
+	// 	if (content[i] >= 'a' && content[i] <= 'z')
+	// 	{
+	// 		content[i] = content[i] - 32;
+	// 	}
+	// 	else if (content[i] >= 'A' && content[i] <= 'Z')
+	// 	{
+	// 		content[i] = content[i] + 32;
+	// 	}
+	// 
+	// }
+	// 
+	// printf("%s", content);
+
+#pragma endregion
+
+
+	return 0;
+}
