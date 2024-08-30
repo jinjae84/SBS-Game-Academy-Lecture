af0174cb7366a7848a7ebfd348bd345c4f8c7fc5
jinjae84 <jinjae84@naver.com>
2024-08-30 21:46:18 +0900

    (2024.08.30) 랜덤 함수

--- Program/source.c
+++ Program/source.c
@@ -1,85 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-struct GameObject
+int Add(int x, int y)
 {
-	char grade;     // 1 byte
-	int x;          // 4 byte
-	double scale;   // 8 byte
+	return x + y;
+}
 
-	// 구조체 크기의 경우 멤버 변수의 순서에 따라 메모리의
-	// 크기가 다르게 설정될 수 있으며, 구조체 크기를 결정하는
-	// 형태는 기본 자료형으로만 구성됩니다.
-
-};
-
-struct Node
+int Substract(int x, int y)
 {
-	int data;
-	struct Node* next;
-	struct Node;
-};
+	return x - y;
+}
 
-inline void OnCollisionEnter()
+int Multiply(int x, int y)
 {
-	printf("Collision\n");
+	return x * y;
+}
+
+int Divide(int x, int y)
+{
+	return x / y;
+}
+
+int Calculator(int x, int y, int (*fptr)(int, int))
+{
+	return fptr(x, y);
 }
 
 int main()
 {
-#pragma region 바이트 패딩
-	// 멤버 변수를 메모리에서 CPU로 읽을 때 한 번에
-	// 읽을 수 있도록, 컴파일러가 레지스터의 블록에
-	// 맞추어 바이트를 패딩해주는 최적화 작업입니다.
+#pragma region 함수 포인터
+	// 함수의 주소값을 저장하고 가리킬 수 있는
+	// 변수입니다.
 
-	// struct GameObject gameobject;
+	// int (*fptr)(int, int);
 	// 
-	// printf("GameObject의 크기 : %d\n", sizeof(gameobject));
+	// fptr = Add;
+	// 
+	// printf("fptr로 호출한 값 : %d\n", fptr(10, 20));
+	// 
+	// fptr = Substract;
+	// 
+	// printf("fptr로 호출한 값 : %d\n", fptr(10, 20));
 
-	// 구조체의 크기는 구성체를 구성하는 멤버 중에서 크기가
-	// 가장 큰 자료형의 배수가 되도록 정렬합니다.
+	// 함수의 포인터는 함수의 반환형과 매개 변수의 타입이
+	// 일치해야 하며, 함수 포인터를 사용하여 동적으로
+	// 메모리를 할당할 수 있습니다.
+
+	// printf("Calculator의 값 : %d\n", Calculator(5,5,Multiply));
+
+	// 함수의 호출은 컴파일 시점에 결정되지만, 함수 포인터로
+	// 함수를 호출하게 되면 프로그램이 실행되는 시점에 함수의
+	// 이름을 운영체제에 전달하여 함수를 호출할 수 있습니다.
 
 #pragma endregion
 
-#pragma region 인라인 함수
-	// 함수를 호출하는 대신 함수가 호출되는 위치마다
-	// 함수의 코드를 복사하여 전달하는 방식의 함수입니다.
+#pragma region rand 함수
+	// 0 ~ 32767 사이의 난수 값을 반환하는 함수입니다.
 
-	//OnCollisionEnter();
-	
-	// 인라인 함수는 함수를 호출하는 과정이 없으므로 처리
-	// 속도가 빠르지만, 인라인 함수를 많이 사용하게 되면
-	// 함수의 코드가 복사되기 때문에 실행 파일의 크기가 커지게 됩니다.
+
+	// UTC 기준으로 1970년 1월 1일 0시 0분 0초부터 경과된
+	// 시간을 초(sec)로 반환하는 함수입니다.
+	// srand(time(NULL));
+	// 
+	// int random = rand() % 10 + 1;
+	// 
+	// printf("random 변수의 값 : %d\n", random);
 
 #pragma endregion
 
-#pragma region 구조체 포인터
+#pragma region UP - DOWN 게임
 
-	// struct Node * node = malloc(sizeof(struct Node));
-	// 
-	// node->data = 100;
-	// 
-	// printf("node1의 data 값 : %d\n", node->data);
-	// 
-	// free(node);
+	int life = 5;
 
-#pragma endregion
+	srand(time(NULL));
 
-#pragma region 자기 참여 구조체
+	int radom = rand() % 50 + 1;
 
-	struct Node node1 = { 10, NULL };
-	struct Node node2 = { 20, NULL };
-	struct Node node3 = { 30, NULL };
-
-	node1.next = &node2;
-	node2.next = &node3;
-	node3.next = NULL;
-
-	int d[] = {node1.data, node2.data, node3.data, node3.next}
-	
-	int * d;
-
-	printf("node 데이터의 값 : %d\n", current)
+	while (scanf_s != srand)
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			printf("♥");
+		}
+	}
 
 #pragma endregion
 
